536f7173f197fb3377d909a2a72da908fd803f71
DarrickAZ <zhwwhx@126.com>
2018-03-09 14:38:16 +0800

    ### add fot sign tip.

---
+++ app/src/main/java/com/coolweather/android/util/MailUtil.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.util;
+
+/**
+ * @author Darrick
+ * @package com.coolweather.android.util
+ * @class MailUtil
+ * @date 2018/3/9 14:24
+ * @description 邮件发送工具类
+ * @versions 1.0
+ */
+public class MailUtil {
+
+
+
+
+
+}
--- app/src/test/java/com/coolweather/android/ExampleUnitTest.java
+++ app/src/test/java/com/coolweather/android/ExampleUnitTest.java
@@ -1,6 +1,21 @@
 package com.coolweather.android;
 
+import com.sun.mail.util.MailSSLSocketFactory;
+
 import org.junit.Test;
+
+import java.security.GeneralSecurityException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import static org.junit.Assert.*;
 
@@ -14,4 +29,86 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void testSendMailByQQ() throws GeneralSecurityException, MessagingException {
+                    Properties props = new Properties();
+                    // 开启debug调试
+                    props.setProperty("mail.debug", "true");
+                    // 发送服务器需要身份验证
+                    props.setProperty("mail.smtp.auth", "true");
+                    // 设置邮件服务器主机名
+                    props.setProperty("mail.host", "smtp.qq.com");
+                    // 发送邮件协议名称
+                    props.setProperty("mail.transport.protocol", "smtp");
+
+                    //qq邮箱用于ssl加密
+                    MailSSLSocketFactory sf = new MailSSLSocketFactory();
+                    sf.setTrustAllHosts(true);
+                    props.put("mail.smtp.ssl.enable", "true");
+                    props.put("mail.smtp.ssl.socketFactory", sf);
+
+                    // 设置环境信息
+                    Session session = Session.getInstance(props);
+
+                    // 创建邮件对象
+                    Message msg = new MimeMessage(session);
+                    msg.setSubject("JavaMail测试");
+                    // 设置邮件内容
+                    msg.setText("这是一封由JavaMail发送的邮件！");
+                    // 设置发件人
+                    msg.setFrom(new InternetAddress("2171651923@qq.com"));
+
+                    Transport transport = session.getTransport();
+                    // 连接邮件服务器以及账户、smtp授权码 pmjtrxzpdiateaed
+                    transport.connect("smtp.qq.com","2171651923@qq.com", "pmjt rxzp diat eaed");
+                    // 发送邮件
+                    transport.sendMessage(msg, new Address[] { new InternetAddress("zhangweiwei@collabtech.cn") });
+                    // 关闭连接
+                    transport.close();
+
+    }
+
+    @Test
+    public void testSendMailBy163() throws GeneralSecurityException, MessagingException {
+        Properties props = new Properties();
+        // 开启debug调试
+        props.setProperty("mail.debug", "true");
+        // 发送服务器需要身份验证
+        props.setProperty("mail.smtp.auth", "true");
+        // 设置邮件服务器主机名
+        props.setProperty("mail.host", "smtp.163.com");
+        // 发送邮件协议名称
+        props.setProperty("mail.transport.protocol", "smtp");
+
+        //qq邮箱用于ssl加密
+        MailSSLSocketFactory sf = new MailSSLSocketFactory();
+        sf.setTrustAllHosts(true);
+        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.ssl.socketFactory", sf);
+
+        // 设置环境信息
+        Session session = Session.getInstance(props);
+
+        // 创建邮件对象
+        Message msg = new MimeMessage(session);
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String d = sdf.format(date);
+        msg.setSubject("JavaMail测试----"+d);
+        // 设置邮件内容
+        msg.setText("这是一封由JavaMail发送的邮件！");
+        // 设置发件人
+        msg.setFrom(new InternetAddress("13018006329@163.com"));
+
+        Transport transport = session.getTransport();
+        // 连接邮件服务器以及账户、smtp授权码 pmjtrxzpdiateaed
+        transport.connect("smtp.163.com","13018006329@163.com", "tjq101zww723");
+        // 发送邮件
+        transport.sendMessage(msg, new Address[] { new InternetAddress("zhangweiwei@collabtech.cn") });
+        // 关闭连接
+        transport.close();
+
+    }
+
 }
